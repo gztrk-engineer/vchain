b671a6906e2a0a94172f9f1e4b3bce9e08e9064e
gztrk-engineer <gztrk@engineer.com>
2023-05-08 15:14:12 +0400

    update the sha function

--- crypto_hash.py
+++ crypto_hash.py
@@ -9,10 +9,10 @@ def cryptoHash(*args):
     """
     # stringifiedData = json.dumps(args)
     # stringifiedArgs = map(stringify, args)
-    stringifiedArgs = map(lambda data: json.dumps(data), args)
+    stringifiedArgs = sorted(map(lambda data: json.dumps(data), args))
     # print(f'args: {args}')
     # joinedData = "".join(args)
-    print(f'args: {stringifiedArgs}')
+    # print(f'args: {stringifiedArgs}')
     joinedData = "".join(stringifiedArgs)
     # print(f'joinedData: {joinedData}')
 
@@ -20,6 +20,7 @@ def cryptoHash(*args):
 
 def main():
     print( f'Cryptohash ("one", 2, [3]): {cryptoHash("one", 2, [3])}' )
+    print( f'Cryptohash (2, "one", [3]): {cryptoHash(2, "one", [3])}' )
 
 if __name__ == "__main__":
     main()
